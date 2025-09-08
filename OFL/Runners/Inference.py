9a5981a4eeef452cec7cdc3fde27d34df15f098b
shadadu <sxantwi@gmail.com>
2025-09-08 14:15:48 -0400

    Create Inference.py

---
+++ OFL/Runners/Inference.py
@@ -0,0 +1,32 @@
+from OFL.Predictors.Categories import get_osm_category, get_foursquare_category
+from OFL.Predictors.Predictors import get_fsq_count, get_median_income_by_point, generate_circle_points
+from OFL.Helpers import get_osm_poi_density, get_population_density_gee, get_fips_from_coords, snap_to_nearest_town
+from OFL.Predictors.Predictors import build_features_for_location, generate_city_candidate_locations
+from OFL.Helpers import _get_duckdb_connection
+
+
+
+
+def build_inference_features_for_location(lat, lon, radius_m, cr, _fsq_duckdb_con, _fsq_query_cache, CENSUS_API_KEY):
+    neighborhood_points = generate_circle_points(lat, lon, radius_m, cr)
+    print(f'Number of neighborhood points {len(neighborhood_points)}')
+    features = []
+    for (lat_i, lon_i) in neighborhood_points:
+        pop = get_population_density_gee(lat_i, lon_i, cr)
+        osm_poi = get_osm_poi_density(lat_i, lon_i, cr)
+        fsq_poi = get_fsq_count(lat_i, lon_i, cr, _fsq_duckdb_con, _fsq_query_cache)
+        income = get_median_income_by_point(lat_i, lon_i, cr, CENSUS_API_KEY)
+        osm_cat = get_osm_category(lat, lon)
+        fsq_cat = get_foursquare_category(lat, lon)
+        features.append({
+            "lat": lat_i,
+            "lon": lon_i,
+            "population_density": pop,
+            "osm_poi_density": osm_poi,
+            "fsq_poi_count": fsq_poi,
+            "median_income": income,
+            "location_category_foursquare": fsq_cat,
+            "location_category_osm": osm_cat,
+        })
+
+    return features
