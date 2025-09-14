e024ac505d69de97cfd3ace4343b19684cd8cba1
shadadu <sxantwi@gmail.com>
2025-09-14 01:09:48 -0400

    Update Inference.py

--- OFL/Runners/Inference.py
+++ OFL/Runners/Inference.py
@@ -10,7 +10,7 @@ def build_inference_features_for_location(lat, lon, radius_m, cr, _fsq_duckdb_co
     features = []
     for (lat_i, lon_i) in neighborhood_points:
         pop = Helpers.get_population_density_gee(lat_i, lon_i, cr)
-        osm_poi = Predictors.get_osm_poi_density(lat_i, lon_i, cr)
+        osm_poi = Helpers.get_osm_poi_density(lat_i, lon_i, cr)
         fsq_poi = Predictors.get_fsq_count(lat_i, lon_i, cr, _fsq_query_cache, _fsq_duckdb_con, )
         income = Predictors.get_median_income_by_point(lat_i, lon_i, cr, census_api_key)
         osm_cat = get_osm_category(lat, lon)
