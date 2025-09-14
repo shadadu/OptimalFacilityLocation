e6574daa553deb98edff2c349f90583b892071b4
shadadu <sxantwi@gmail.com>
2025-09-14 01:09:29 -0400

    Update Train.py

--- OFL/Runners/Train.py
+++ OFL/Runners/Train.py
@@ -1,16 +1,15 @@
-import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-import ee
-from OFL.Predictors.Predictors import generate_city_candidate_locations
 from OFL.Predictors.Categories import encode_location_categories
 import time
-
-
+from huggingface_hub import notebook_login
+import json, ast
+import pickle
 
 
 def build_xy(data_dir_path):
-    df = pd.read_csv(data_dir_path)
+    file = "location_revenue_and_predictors.csv"
+    df = pd.read_csv(data_dir_path + file)
     df_vars = encode_location_categories(df)
     X = df_vars[["population_density"
         , "osm_poi_density"
@@ -18,7 +17,8 @@ def build_xy(data_dir_path):
         , "median_income"
         , "fsq_category_encoded"
         , "osm_category_encoded"]]
-    y = df_vars["revenue"]
+    y = df_vars["revenue"].apply(float)
+    print(f'y {y}')
 
     return X, y
 
@@ -37,9 +37,15 @@ def main():
     """
     Loads the model from csv file and retrieves X and y for model training
     """
-    X, y = build_xy(data_dir_path="")
+    X, y = build_xy(data_dir_path="/Users/rckyi/Documents/Data/")
 
     model = train(X, y, None)
+
+    filename = "/Users/rckyi/Documents/Data/linear_regression_model.pkl"
+    with open(filename, 'wb') as file:
+        pickle.dump(model, file)
+
+
 
 
 if __name__ == "__main__":
@@ -51,5 +57,3 @@ if __name__ == "__main__":
     elapsed_minutes = elapsed_seconds / 60
 
     print(f"Execution time: {elapsed_minutes:.2f} minutes")
-
-
